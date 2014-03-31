e59ac9fcfeabb9f2b9624b8023eac98e087bf34c
Kristian Våge <krisvaage@gmail.com>
2014-04-01 01:11:31 +0200

    Moves a few puts statements

--- db/calendar.rb
+++ db/calendar.rb
@@ -1,18 +1,23 @@
 require_relative 'meetingroom'
 
 def main
+  puts "All:"
   all_test
+  puts "\n\n"
+
+  puts "Id: 1"
   find_test
+
+
+  puts "\n\n"
   find_not_found_test
 end
 
 def all_test
-  puts "All:\n"
   puts Meetingroom.all_string
 end
 
 def find_test
-  puts "\n\nId: 1\n"
   puts Meetingroom.find(1).to_s
 end
 
@@ -20,7 +25,7 @@ def find_not_found_test
   begin
     Meetingroom.find(42)
   rescue RuntimeError => e
-    puts "\n\nDid not find 42"
+    puts "Did not find 42"
     puts e
   end
 end
