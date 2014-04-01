68db2d0ac5ac909c8a029a8e2b974fa9f1fb8335
Kristian Våge <krisvaage@gmail.com>
2014-04-01 12:42:17 +0200

    rails new rails_calendar

---
+++ rails_calendar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsCalendar::Application.config.secret_key_base = '1351112d5b6a898ff9295ac8bce0dc71529f77fc25b108eaa2dc22d099abd22768fe6997054b619470f8c2a475817de4fcbcd632cd681078879e87a1a373f319'
---
+++ rails_calendar/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsCalendar::Application.config.session_store :cookie_store, key: '_rails_calendar_session'
